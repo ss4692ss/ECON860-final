ce0087ce95f24519d32b8698ea960a133dbe6f32
Sanjeev Sharma <143121687+ss4692ss@users.noreply.github.com>
2023-12-10 20:46:36 -0500

    neww

---
+++ assignment.py
@@ -0,0 +1,46 @@
+import pandas
+
+from factor_analyzer import FactorAnalyzer
+
+from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity
+
+import numpy
+
+dataset1 = pandas.read_csv("dataset_final.csv")
+
+
+dataset1 = dataset1.values
+
+dataset1 = dataset1[:,0:38]
+
+print(dataset1)
+
+
+chi2 ,p=calculate_bartlett_sphericity(dataset1)
+print(chi2, p)
+
+machine = FactorAnalyzer(n_factors=38, rotation=None)
+
+machine.fit(dataset1)
+
+ev, v = machine.get_eigenvalues()
+print(ev)
+
+
+
+
+machine = FactorAnalyzer(n_factors=4, rotation=None)
+machine.fit(dataset1)
+output = machine.loadings_
+print(output)
+
+machine = FactorAnalyzer(n_factors=4, rotation='varimax')
+machine.fit(dataset1)
+factor_loadings = machine.loadings_
+print(factor_loadings)
+
+
+results = numpy.dot(dataset1, factor_loadings)
+
+pandas.DataFrame(results).round().to_csv("results.csv", index=False)
+
